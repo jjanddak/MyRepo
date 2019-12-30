5849d1b47a33fba165dd002a51829eb7a6401b14
jonghyeok <opplane@gmail.com>
2019-12-30 10:37:57 +0900

    지운거되돌림

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -4,5 +4,7 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("gura kim");
 		System.out.println("monkey");
+		System.out.println("made in lab1");
+		System.out.println("made in lab1 #2");		
 	}
 }
