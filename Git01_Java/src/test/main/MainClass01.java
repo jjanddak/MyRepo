d081d15250d7f418569e93ab451e43f485a415ac
jonghyeok <opplane@gmail.com>
2019-12-30 10:52:25 +0900

    lab1 hello

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -5,6 +5,7 @@ public class MainClass01 {
 		System.out.println("gura kim");
 		System.out.println("monkey");
 		System.out.println("made in lab1");
-		System.out.println("made in lab1 #2");		
+		System.out.println("made in lab1 #2");
+		System.out.println("lab1 브렌치 hEllOlololo");
 	}
 }
