d3385910402d38983a4b95c8dbec3b058610508e
jonghyeok <opplane@gmail.com>
2019-12-30 10:51:12 +0900

    System.out.println("master ㅂ렌치 Hellolololo"); added

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -5,6 +5,7 @@ public class MainClass01 {
 		System.out.println("gura kim");
 		System.out.println("monkey");
 		System.out.println("made in lab1");
-		System.out.println("made in lab1 #2");		
+		System.out.println("made in lab1 #2");
+		System.out.println("master ㅂ렌치 Hellolololo");
 	}
 }
