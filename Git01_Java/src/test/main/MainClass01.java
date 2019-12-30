d12bc68be68fbbf6bd03208ba3b23138d64c9fb0
jonghyeok <opplane@gmail.com>
2019-12-30 10:08:45 +0900

    lab1에서 madein printed

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -4,5 +4,6 @@ public class MainClass01 {
 	public static void main(String[] args) {
 		System.out.println("gura kim");
 		System.out.println("monkey");
+		System.out.println("made in lab1");
 	}
 }
